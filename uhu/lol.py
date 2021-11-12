76a88553ac4b2141c838ab082473d43e41720d44
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-11-12 18:07:02 +0100

    remove unnecessary folder

--- uhu/lol.py
+++
@@ -1,7 +0,0 @@
-task_to_ws = dict()
-import orjson
-from modules import database
-
-
-async def lol(x, pc):
-    await x[pc].send_text(orjson.dumps(database.status_scheduler(pc).dict()).decode("utf-8"))
