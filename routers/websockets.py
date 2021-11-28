3fb827632a1f675c7783e6a33c76c91cbfe9b6ed
Mohammed Andiche <mohammed.andiche@student.kuleuven.be>
2021-11-28 15:51:04 +0100

    fix tiny error

--- routers/websockets.py
+++ routers/websockets.py
@@ -15,7 +15,7 @@ async def websocket_endpoint_(websocket: WebSocket):
     await websocket.accept()
     task_id = await websocket.receive_text()
     task_to_ws.setdefault(task_id, set())
-    task_to_ws.add(websocket)
+    task_to_ws[task_id].add(websocket)
     await websocket.send_text(udumps(status_task(task_id)))
     while True:
         await Event().wait()
